b4b58d487fed3ae0b32a8e114e02d760100cbee6
Ryan Morales <ryan.morales@west-mec.org>
2024-11-13 08:09:48 +0000

    start of lesson with empty models

--- models/GroceryItem.js
+++ models/GroceryItem.js
@@ -1,32 +1,5 @@
-// models/GroceryItem.js
 const mongoose = require('mongoose');
 
-const grocerySchema = new mongoose.Schema({
-  item: {
-    type: String,
-    required: [true, 'Item name is required'],
-    trim: true
-  },
-  food_group: {
-    type: String,
-    required: [true, 'Food group is required'],
-    enum: ['fruits', 'vegetables', 'proteins', 'dairy', 'grains', 'nuts']
-  },
-  price_in_usd: {
-    type: Number,
-    required: [true, 'Price is required']
-  },
-  quantity: {
-    type: Number,
-    required: [true, 'Quantity is required'],
-    min: 0
-  },
-  calories_per_100g: Number,
-  organic: Boolean,
-  wild_caught: Boolean,
-  fat_content: String,
-  gluten_free: Boolean,
-  free_range: Boolean
-});
+const grocerySchema = new mongoose.Schema({});
 
 module.exports = mongoose.model('GroceryItem', grocerySchema);
