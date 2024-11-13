b6a238146268de9f3515295fc79c0e73b00cf852
Ryan Morales <ryan.morales@west-mec.org>
2024-11-13 08:42:52 +0000

    end of night 11/12

--- models/GroceryItem.js
+++ models/GroceryItem.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
-const grocerySchema = new mongoose.Schema({});
+const grocerySchema = new mongoose.Schema({
+    item: {
+        type: String,
+        required: [true, 'Item name is required'],
+        trim: true
+    },
+    food_group: {
+        type: String,
+        required: [true, 'Food group is required'],
+        enum: ['fruits', 'vegetables', 'proteins', 'dairy', 'grains', 'nuts']
+    },
+});
 
 module.exports = mongoose.model('GroceryItem', grocerySchema);
