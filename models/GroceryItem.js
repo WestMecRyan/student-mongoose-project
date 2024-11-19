d0045d0b28eb848b3012cd55d2d27bf2c10ffec6
Ryan Morales <ryan.morales@west-mec.org>
2024-11-19 00:25:51 +0000

    added grocery item model validation

--- models/GroceryItem.js
+++ models/GroceryItem.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose');
 
-const grocerySchema = new mongoose.Schema({
-    item: {
-        type: String,
-        required: [true, 'item name is required']
-    },
-    food_group: {
-        type: String,
-        required: [true, 'food_group is required'],
-        enum: ['fruits', 'dairy', 'proteins', 'vegetables', 'nuts', 'grains']
-    },
-    price_in_usd: {
-        type: Number,
-        required: [true, 'Please enter a number for price_in_usd'], min: [0, 'Price must be a positive number']
-    }
-});
+const grocerySchema = new mongoose.Schema({});
 
 module.exports = mongoose.model('GroceryItem', grocerySchema);
